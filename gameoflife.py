43afe2025301a51f6627d741a1785b879ae19174
riptoc <riptoc@protonmail.com>
2019-09-04 18:18:16 +0100

    Func to generate gamestate

--- gameoflife.py
+++ gameoflife.py
@@ -4,4 +4,9 @@
 
 
 def generate_gamestate(input):
-    """Generate gamestate as 2d array from input"""
+    """
+        Generate gamestate as 2d array from input
+        eg. "# ## " -> [1,0,1,1,0]
+    """
+    items = [[c for c in i] for i in input]
+    return [[1 if i == "#" else 0 for i in j] for j in items]
